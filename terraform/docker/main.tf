02ab7b8b1b8c87bd5399070651ef15c98eceaf70
jonathanpereiram <jonathan.pereira.matus@gmail.com>
2022-09-28 15:30:37 -0300

    setup docker in terraform

--- terraform/docker/main.tf
+++ terraform/docker/main.tf
@@ -14,22 +14,55 @@ provider "docker" {
 # images
 
 resource "docker_image" "jenkins" {
-  name         = "jenkins:lts-jdk11"
+  name         = "jenkins/jenkins:lts-jdk11"
   keep_locally = false
+}
+
+resource "docker_image" "remote-host" {
+  name = "remote-host"
+  build {
+    path = "../../docker/centos7"
+    tag  = ["develop"]
+  }
 }
 
 # containers
 
 resource "docker_container" "jenkins" {
-  image = docker_image.jenkins.name
   name  = "jenkins"
+  image = docker_image.jenkins.name
   ports {
     internal = 8080
     external = 8080
   }
 
   volumes {
+    volume_name    = docker_volume.jenkins.name
     container_path = "/var/jenkins_home"
-    host_path = "../../docker/jenkins/jenkins_home"
   }
+
+  networks_advanced {
+    name = docker_network.net.name
+  }
+}
+
+resource "docker_container" "remote-host" {
+  name  = "remote-host"
+  image = docker_image.remote-host.name
+
+  networks_advanced {
+    name = docker_network.net.name
+  }
+}
+
+# volumes
+
+resource "docker_volume" "jenkins" {
+  name = "jenkins"
+}
+
+# networks
+
+resource "docker_network" "net" {
+  name = "net"
 }
